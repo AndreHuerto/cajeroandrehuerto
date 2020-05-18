a4d6ff15a552cd547cc29c7bb77b090c9d692793
André <diegohuerto@upeu.edu.pe>
2020-05-18 11:01:41 -0500

    primer avanze

---
+++ JS/numeros.js
@@ -0,0 +1,28 @@
+document.getElementById("n1").addEventListener("click",n1);
+function n1(){
+    let sumado = document.getElementById("n1").innerHTML;
+    var elemento = document.getElementById("caja-password");                                                        
+}
+
+document.getElementById("n2").addEventListener("click",n2);
+
+document.getElementById("n3").addEventListener("click",n3);
+
+document.getElementById("n4").addEventListener("click",n4);
+
+document.getElementById("n5").addEventListener("click",n5);
+
+document.getElementById("n6").addEventListener("click",n6);
+
+document.getElementById("n7").addEventListener("click",n7);
+
+document.getElementById("n8").addEventListener("click",n8);
+
+document.getElementById("n9").addEventListener("click",n9);
+
+document.getElementById("n10").addEventListener("click",n10);
+
+document.getElementById("n11").addEventListener("click",n11);
+
+document.getElementById("n12").addEventListener("click",n12);
+
